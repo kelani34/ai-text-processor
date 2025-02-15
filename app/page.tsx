fc82ece19cc4fdc96f70182d4ddf8a733a3b8cd6
Shina-Kelani Taiwo <kelanitaiwo34@gmail.com>
2025-02-15 19:32:14 +0100

    update: add test ai browser prompt

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,25 @@
-import Image from 'next/image';
+'use client';
+
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    (async () => {
+      const session = await window.ai.languageModel.create();
+
+      // Prompt the model and wait for the whole result to come back.
+      const result = await session.prompt('Write me a poem.');
+      console.log(result);
+
+      // Prompt the model and stream the result:
+      const stream = await session.promptStreaming(
+        'Write me an extra-long poem.'
+      );
+      for await (const chunk of stream) {
+        console.log(chunk);
+      }
+    })();
+  }, []);
+
   return <main></main>;
 }
